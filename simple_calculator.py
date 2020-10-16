eee19a38d04543fc9bc4418145f5143c94c41cb7
Arasch Askari <33096218+arascch@users.noreply.github.com>
2020-10-16 15:08:32 +0330

    user add number for calculation

--- simple_calculator.py
+++ simple_calculator.py
@@ -2,3 +2,14 @@
 
 operation = input("would you like to add/subtract/multiply/divide?").lower()
 print ("you chose {}.".format(operation))
+
+#ask for numbers to calculate
+
+if operation =="subtract" or operation=="divide":
+    print("you chose{}.".format(operation))
+    print("please keep in mind that the order of your number matter")
+
+num1 = input("what is the first number?")
+num2 = input("what is the second number?")
+print("first number:{}".format(num1))
+print("the second number:{}".format(num2))
