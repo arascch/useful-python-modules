b61e941a6602ed7c5c827cd452a02edf0224d2f8
Arasch Askari <33096218+arascch@users.noreply.github.com>
2020-11-29 15:21:52 +0330

    create blackjack class and create the makedeck function

--- blackjack_game.py
+++ blackjack_game.py
@@ -1,2 +1,20 @@
 #the simple blackjack game 
 
+from random import randint
+from IPython.display import clear_output
+
+class blackjack():
+    def __init__(self):
+        self.deck=[]
+        self.suits = ("spades" , "hearts" , "diamonds" , "clubs")
+        self.values = (2,3,4,5,6,7,8,9,10,"J","Q","K","A")
+
+    def makeDeck(self):
+        for suit in self.suits:
+            for value in self.values:
+                self.deck.append((value , suit))
+
+game = blackjack()
+game.makeDeck()
+print(game.deck)
+
