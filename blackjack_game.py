f33d0430ec0ce2a6fd402877f3d3f8640228462c
Arasch Askari <33096218+arascch@users.noreply.github.com>
2020-11-30 12:16:17 +0330

    add function calchand

--- blackjack_game.py
+++ blackjack_game.py
@@ -35,7 +35,27 @@ class Player():
             else:
                 card = self.hand[ i ]
                 print("{} of {}".format(card[0] , card[1]))
+            print ("Total = {}".format(self.calcHand(dealer_start)))
 
+#if not dealer's turn give back total of second card
+def calcHand(self , dealer_start=True):
+    total = 0
+    aces = 0
+    card_values = {1:1 , 2:2 , 3:3 , 4:4 , 5:5 , 6:6 , 7:7 , 8:8 , 9:9, 10:10 , "J":10 , "Q":10 , "K":10,"A":11}
+    if self.name == "dealer" and dealer_start:
+        card = self.hand[1]
+        return card_values[card[0]]
+    for card in self.hand:
+        if card[0] == "A":
+            aces +=1
+        else:
+            total += card_values[card[0]]
+    for i in range(aces):
+        if total+11>21:
+            total+=1
+        else: 
+            total+=11
+    return total
 game = blackjack()
 game.makeDeck()
 name = input("whats your name?")
