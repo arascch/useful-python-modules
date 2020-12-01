e9101bf8c3d293b86f6e589eb14c9940ffbf8c76
Arasch Askari <33096218+arascch@users.noreply.github.com>
2020-12-01 15:13:02 +0330

    ok

--- binary_Search.py
+++ binary_Search.py
@@ -3,4 +3,16 @@ import random
 
 nums = [random.randint(0 ,20) for i in range(10)]
 
+def binarySearch(aList , nums):
+    aList.sort()
+    while aList:
+        mid = len(aList)//2
+        if aList[mid] == nums:
+            return True
+        elif aList[mid]>nums:
+            aList = aList[:mid]
+        elif aList[mid]<nums:
+            aList = aList[mid+1:]
+    return False
 print(sorted(nums))
+print(binarySearch(nums , 3))
