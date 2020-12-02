688905e2a97399467fec0e377c81933ac3b0b543
Arasch Askari <33096218+arascch@users.noreply.github.com>
2020-12-02 13:13:42 +0330

    change the name of programs and sorted by difficulty

--- 7-factorial_simple.py
+++ 7-factorial_simple.py
@@ -3,5 +3,6 @@ def factorial(n):
         return 1 
     else:
         return factorial(n-1)*n
+##for calculate the number in your mind you can change the below number
 
 print(factorial(10))
--- 8-fibonacco_app.py
+++ 8-fibonacco_app.py
@@ -1,7 +1,8 @@
+
 def fib(n):
     if n<=1:
         return n
     else:
         return fib(n-1) + fib(n-2)
-
+#for calculate the number in your mind you can change the below number
 print(fib(10))
