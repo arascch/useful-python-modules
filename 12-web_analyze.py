31b21cdfe15c77fc74b2ae3b2a4a60a482a98207
Arasch Askari <33096218+arascch@users.noreply.github.com>
2020-12-09 22:32:43 +0330

    add scrape function

--- 12-web_analyze.py
+++ 12-web_analyze.py
@@ -4,11 +4,20 @@ from bs4 import BeautifulSoup
 from bs4.element import Comment
 from Ipython.display import clear_output
 
+def scrape(title):
+    page = requests.get(site)
+    soup = BeautifulSoup(page.content , "html.parser")
+    print(soup.prettify())
+
+    
 while input("what you like to scrape a website(y/n)?")=="y":
     try:
         clear_output()
         site = input("enter a site you want for analyze")
-        print(site)
+        scrape(site)
     except:
         print("something went wrong , please try again.")
 print("Thanks for using our analyzer:D")
+
+#request for return most used words 
+
