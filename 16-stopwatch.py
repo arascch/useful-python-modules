687d432f8701801d684790e16a067f2bb20080a6
arash askari <arash.ask.94@gmail.com>
2021-07-06 09:20:55 +0430

    add modules and lable function

---
+++ 16-stopwatch.py
@@ -0,0 +1,25 @@
+import tkinter
+from datetime import datetime
+
+counter = 66600
+running = False
+
+def counter_label(label):
+    def count():
+        if running:
+            global counter
+
+            if counter == 66600:
+                display = "Starting ..."
+            else:
+                tt = datetime.fromtimestamp(counter)
+                string = tt.strftime("%H:%M:%S")
+                display = string 
+            
+            label['text'] = display
+
+            label.after(1000,count)
+            counter+=1
+    count()
+
+ 
