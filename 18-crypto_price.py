24b89fb234ace3c7ec42d83a959cc5bb0fa762d3
Arash askari <arasch.ask@gmail.com>
2025-11-01 10:53:22 +0330

    add cryptoprice script

---
+++ 18-crypto_price.py
@@ -0,0 +1,16 @@
+import json
+import requests
+
+#binance APi URL
+key = "https://api.binance.com/api/v3/ticker/price?symbol="
+
+currencies = ["BTCUSDT" , "DOGEUSDT"]
+
+j = 0
+
+for i in currencies:
+    url = key+currencies[j]
+    data = requests.get(url)
+    data = data.json()
+    j = j+1
+    print(f"{data['symbol']} price is {data['price']}")
